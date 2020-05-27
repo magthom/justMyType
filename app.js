9ac29103e7d91efd8b5488abd01cea3fdc23b6aa
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-27 15:00:59 -0500

    toggle between keyboards on shiftkey

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-    $('#keyboard-upper-container').hide();
+    $("#keyboard-upper-container").hide();
+
+    $(document).keydown(function(e) {
+        if (e.keyCode === 16) {
+          $("#keyboard-upper-container").show();
+          $("#keyboard-lower-container").hide();
+        }
+      });
+      $(document).keyup(function(e) {
+        if (e.keyCode === 16) {
+          $("#keyboard-upper-container").hide();
+          $("#keyboard-lower-container").show();
+        }
+        });
+
+  
 });
