30444a42479d1a634b85b1b0b4d52cbe5650afd5
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-28 14:46:28 -0500

    one sentence is visible on page load

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {    
     $("#keyboard-upper-container").hide();
 
-    $(document).keydown(function(e) {
-        if (e.keyCode === 16) {
-          $("#keyboard-upper-container").show();
-          $("#keyboard-lower-container").hide();
-        }
-      });
-      $(document).keyup(function(e) {
-        if (e.keyCode === 16) {
-          $("#keyboard-upper-container").hide();
-          $("#keyboard-lower-container").show();
-        }
-        });
+    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 
+    'Too ato too nOt enot one totA not anot tOO aNot', 
+    'oat itain oat tain nate eate tea anne inant nean', 
+    'itant eate anot eat nato inate eat anot tain eat', 
+    'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
-  
+    $("#sentence").append(sentences[0]);
+
+   
+    
+   
+
+
+    $(document).keydown(function (e) {
+        if (e.keyCode === 16) {
+            $("#keyboard-upper-container").show();
+            $("#keyboard-lower-container").hide();
+        }
+    });
+    $(document).keyup(function (e) {
+        if (e.keyCode === 16) {
+            $("#keyboard-upper-container").hide();
+            $("#keyboard-lower-container").show();
+        }
+        //$(".highlight").removeClass("highlight")
+    });
+   
+
 });
